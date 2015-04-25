ddb0ae4b64314da6e97f85338da8887f45d97b6f
Mihir Singh <git.service@mihirsingh.com>
2015-04-25 14:41:26 -0500

    Retrieved heart rate data for the past day

--- Rotule/ViewController.swift
+++ Rotule/ViewController.swift
@@ -7,12 +7,43 @@
 //
 
 import UIKit
+import HealthKit
 
 class ViewController: UIViewController {
 
+    let healthKitStore:HKHealthStore = HKHealthStore()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        let healthKitTypesToRead = Set([
+            HKObjectType.quantityTypeForIdentifier(HKQuantityTypeIdentifierHeartRate)
+        ])
+
+        healthKitStore.requestAuthorizationToShareTypes(nil, readTypes: healthKitTypesToRead, completion: nil)
+
+        let endDate = NSDate()
+        let startDate = NSCalendar.currentCalendar().dateByAddingUnit(.CalendarUnitDay,
+            value: -1, toDate: endDate, options: nil)
+
+        let heartRateSampleType = HKSampleType.quantityTypeForIdentifier(HKQuantityTypeIdentifierHeartRate)
+        let predicate = HKQuery.predicateForSamplesWithStartDate(startDate,
+            endDate: endDate, options: .None)
+
+        let query = HKSampleQuery(sampleType: heartRateSampleType, predicate: predicate,
+            limit: 0, sortDescriptors: nil, resultsHandler: {
+                (query, results, error) in
+                if error != nil {
+                    println("There was an error running the query: \(error)")
+                }
+                dispatch_async(dispatch_get_main_queue()) {
+                    for result in results {
+                        println(result.quantity?.doubleValueForUnit(HKUnit(fromString: "count/min")))
+                    }
+                }
+        })
+
+        healthKitStore.executeQuery(query)
     }
 
     override func didReceiveMemoryWarning() {
